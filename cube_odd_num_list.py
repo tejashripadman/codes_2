5ef2a65f4c76acc93a3caf74aa1f0335eee7af24
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-08-05 21:07:21 +0530

    Update cube_odd_num_list.py

--- cube_odd_num_list.py
+++ cube_odd_num_list.py
@@ -1,7 +1,8 @@
 def cube(n):
-  return lambda n: n**3
+    return n**3
 def odd(n):
-  return n if n%2
+    if n % 2:
+        return n
 def cube_odd_num(l):
-  return list(filter(odd,map(cube,l)))
+    return list(filter(odd,map(cube,l)))
   
