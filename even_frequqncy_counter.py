320d799c5e733e237b4d78bef9961286efc25bd0
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-08-12 09:57:55 +0530

    Create even_frequqncy_counter.py

---
+++ even_frequqncy_counter.py
@@ -0,0 +1,4 @@
+def even_freq(s):
+    """This function takes string as input and returns list with the characters having even frequency in string"""
+    j = [i for i in set(s) if s.count(i)%2 == 0]
+    return(j)
