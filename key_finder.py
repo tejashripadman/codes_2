feed849ad4acb76c3545b0f046ce180197e846b7
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-08-17 20:02:52 +0530

    Update key_finder.py

--- key_finder.py
+++ key_finder.py
@@ -2,7 +2,7 @@ def key_finder(dictionary,key):
     '''This function takes list of dictionary and key as argument
     It returns a list of dictionary which contains given key'''
     l = []
-    for i in test_list:
+    for i in dictionary:
         if key in i.keys():
             l.append(i)
     return l
